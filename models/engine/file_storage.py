141fc5dcf96dc65a2ab7ff53fc4f96f634f31a0d
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-18 16:59:57 +0100

    task 5 delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
+from ast import Not
 import json
+import re
 
 
 class FileStorage:
@@ -8,8 +10,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            new_dict = {}
+            for k, v in self.__objects.items():
+                if type(v) == cls:
+                    new_dict[k] = v
+            return new_dict
         return FileStorage.__objects
 
     def new(self, obj):
@@ -45,6 +53,16 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside - if obj is equal to None
+        the method should not do anything"""
+        if obj is not None:
+            cls = obj.__class__.__name__
+            id = obj.id
+            key = cls+"."+id
+            if key in self.__objects:
+                del self.__objects[key]
