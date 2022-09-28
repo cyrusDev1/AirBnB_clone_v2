ae941558f81e7d9c456cf641a74164b63f8ebc7e
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-28 18:36:58 +0100

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-from fabric.api import 
+from fabric.operations import local
+from datetime import datetime
+
+
+def do_pack():
+    """Function to compares"""
+    local("mkdir -p versions")
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static".format(
+        time))
+    if result.failed:
+        return None
+    return result
