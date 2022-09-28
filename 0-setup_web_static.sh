8fe18eadec1d85812df6e1db3e67ad2f6e1c31e0
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-28 16:28:10 +0100

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+# script that sets up your web servers for the deployment of web_static
+
+apt-get -y update
+apt-get -y install nginx
+
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+HBNB_STATIC="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
+sed -i "29i\ $HBNB_STATIC" /etc/nginx/sites-available/default
+service nginx restart
+
