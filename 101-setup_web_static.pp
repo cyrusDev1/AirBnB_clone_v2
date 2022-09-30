4a223516db2f95bf1d87633853939e4445c61144
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-30 17:01:48 +0100

    Redo the task #0 but by using Puppet:

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,16 +1,29 @@
 # Redo the task #0 but by using Puppet:
 
-exec { 'command':
-    command => "apt-get -y update;
-    apt-get -y install nginx;
-    mkdir -p /data/web_static/;
-    mkdir -p /data/web_static/shared/;
-    mkdir -p /data/web_static/releases/test/;
-    echo Holberton School" > /data/web_static/releases/test/index.html;
-    ln -sf /data/web_static/releases/test/ /data/web_static/current;
-    chown -R ubuntu:ubuntu /data/;
-    HBNB_STATIC='\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}';
-    sed -i '29i\ $HBNB_STATIC' /etc/nginx/sites-available/default;
-    service nginx restart',
-    provider => shell,
+exec { 'a':
+  command => '/usr/bin/env apt-get -y update',
+}
+-> exec {'b':
+  command => '/usr/bin/env apt-get -y install nginx',
+}
+-> exec {'c':
+  command => '/usr/bin/env mkdir -p /data/web_static/releases/test/',
+}
+-> exec {'d':
+  command => '/usr/bin/env mkdir -p /data/web_static/shared/',
+}
+-> exec {'e':
+  command => '/usr/bin/env echo "Puppet x Holberton School" > /data/web_static/releases/test/index.html',
+}
+-> exec {'f':
+  command => '/usr/bin/env ln -sf /data/web_static/releases/test /data/web_static/current',
+}
+-> exec {'g':
+  command => '/usr/bin/env sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
+}
+-> exec {'h':
+  command => '/usr/bin/env chown -R ubuntu:ubuntu /data',
+}
+-> exec {'i':
+  command => '/usr/bin/env service nginx restart',
 }
